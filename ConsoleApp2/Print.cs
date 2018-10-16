fd7c9e1ad963db99e70c514c48bc854850ce53cb
Everton <everton.srs@gmail.com>
2018-10-16 18:18:04 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp2/Print.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using System.Drawing;
+using System.IO;
+using Tesseract;
+
+namespace ConsoleApp2
+{
+    public class Print
+    {
+        public void GenerateSnapshot(string filePath)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize(); driver.Navigate().GoToUrl("http://gps.receita.fazenda.gov.br/");
+
+            var remElement = driver.FindElement(By.Id("captcha_challenge"));
+            Point location = remElement.Location;
+
+            var screenshot = (driver as ChromeDriver).GetScreenshot();
+            using (MemoryStream stream = new MemoryStream(screenshot.AsByteArray))
+            {
+                using (Bitmap bitmap = new Bitmap(stream))
+                {
+                    RectangleF part = new RectangleF(location.X, location.Y, remElement.Size.Width, remElement.Size.Height);
+                    using (Bitmap bn = bitmap.Clone(part, bitmap.PixelFormat))
+                    {
+                        bn.Save(filePath + "CaptchImage.png", System.Drawing.Imaging.ImageFormat.Png);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using System.Drawing;
+using System.IO;
+using Tesseract;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:\\Users\\A36372\\Desktop\\Print\\";
+
+            var teste = new Print();
+
+            teste.GenerateSnapshot(filePath);        
+            
+            
+
+            using (var engine = new TesseractEngine("C:\\Program Files (x86)\\Tesseract-OCR\\tessdata", "por", EngineMode.Default))
+            {         
+
+                Page ocrPage = engine.Process(Pix.LoadFromFile(filePath + "CaptchImage.png"), PageSegMode.AutoOnly);
+                var captchatext = ocrPage.GetText().Trim();
+                Console.WriteLine(captchatext.Replace("\n", "\r\n"));
+                
+            }
+
+            Console.ReadKey();
+
+
+
+        }  
+
+
+
+        
+    }
+}
